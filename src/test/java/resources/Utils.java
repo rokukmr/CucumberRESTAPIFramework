2b478c906af9e4c56d4d075acb38da0acef7ada7
Rohit <54813436+rokukmr@users.noreply.github.com>
2022-12-04 09:15:34 +0530

    Update Utils.java

--- src/test/java/resources/Utils.java
+++ src/test/java/resources/Utils.java
@@ -10,7 +10,6 @@ import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 import io.restassured.specification.ResponseSpecification;
-import jdk.nashorn.internal.ir.RuntimeNode;
 
 import java.io.*;
 import java.util.Properties;
