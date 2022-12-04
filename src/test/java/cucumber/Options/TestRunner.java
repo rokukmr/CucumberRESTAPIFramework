642ff0684612ffd8e82083313a744dad717a5a9b
Rohit <54813436+rokukmr@users.noreply.github.com>
2022-12-04 09:41:38 +0530

    Update TestRunner.java

--- src/test/java/cucumber/Options/TestRunner.java
+++ src/test/java/cucumber/Options/TestRunner.java
@@ -2,6 +2,7 @@ package cucumber.Options;
 
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
+import org.testng.annotations.AfterClass;
 
 @CucumberOptions(features="src/test/java/features", glue="stepDefinations"
 ,plugin = "json:target/jsonReports/cucumber-report.json")
